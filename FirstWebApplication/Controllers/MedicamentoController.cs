034dbc75fe5f061fd8fdc8e42bfa08ba06f9ead2
jonatan guillen <jguillen@viamatica.me>
2023-11-29 17:10:05 -0500

    Agregar archivos de proyecto.

---
+++ FirstWebApplication/Controllers/MedicamentoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class MedicamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+    }
+}
---
+++ FirstWebApplication/Controllers/TipoMedicamentoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApplication.Controllers
+{
+    public class TipoMedicamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+       
+        public IActionResult SinMenu()
+        {
+            return View();
+        }
+
+    }
+}
